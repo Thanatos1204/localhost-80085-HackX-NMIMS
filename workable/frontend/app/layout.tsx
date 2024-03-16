767535aa7658ae8fde91e2947e0c03b41499f4ec
Thanatos1204 <bhargavpandit01@gmail.com>
2024-03-16 14:24:32 +0530

    landing-1

--- workable/frontend/app/layout.tsx
+++ workable/frontend/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Alice } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const alice = Alice({ 
+  weight: '400', 
+  subsets: ['cyrillic']
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={alice.className}>{children}</body>
     </html>
   );
 }
